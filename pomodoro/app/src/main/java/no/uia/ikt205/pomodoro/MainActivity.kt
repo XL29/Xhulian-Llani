ca3204a3ceaa8b76bfdb406a6eee0eccbda622b1
Xhulian Llani <xhulianl@uia.no>
2021-01-25 21:54:11 +0100

    added different timer functionalities and fixed bug

--- pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -13,16 +13,46 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var timer:CountDownTimer
     lateinit var startButton:Button
+    lateinit var ThirtyButton:Button
+    lateinit var SixtyButton:Button
+    lateinit var NinetyButton:Button
+    lateinit var HnTwentyButton:Button
     lateinit var coutdownDisplay:TextView
 
-    val timeToCountDownInMs = 5000L
+    var timeToCountDownInMs = 5000L
     val timeTicks = 1000L
+    var bool = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
        startButton = findViewById<Button>(R.id.startCountdownButton)
+        ThirtyButton = findViewById<Button>(R.id.button_30)
+        SixtyButton = findViewById<Button>(R.id.button_60)
+        NinetyButton = findViewById<Button>(R.id.button_90)
+        HnTwentyButton = findViewById<Button>(R.id.button_120)
+
+        ThirtyButton.setOnClickListener(){
+            timeToCountDownInMs = 1800000L
+            Toast.makeText(this@MainActivity,"Endret tid til 30 min", Toast.LENGTH_SHORT).show()
+        }
+
+        SixtyButton.setOnClickListener(){
+            timeToCountDownInMs = 3600000L
+            Toast.makeText(this@MainActivity,"Endret tid til 60 min", Toast.LENGTH_SHORT).show()
+        }
+
+        NinetyButton.setOnClickListener(){
+            timeToCountDownInMs = 5400000L
+            Toast.makeText(this@MainActivity,"Endret tid til 90 min", Toast.LENGTH_SHORT).show()
+        }
+
+        HnTwentyButton.setOnClickListener(){
+            timeToCountDownInMs = 7200000L
+            Toast.makeText(this@MainActivity,"Endret tid til 120 min", Toast.LENGTH_SHORT).show()
+        }
+
        startButton.setOnClickListener(){
            startCountDown(it)
        }
@@ -31,10 +61,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun startCountDown(v: View){
+        if(bool == true){
+            timer.cancel()
+        }
+
+        bool = true
 
         timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
             override fun onFinish() {
                 Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
+                bool = false
             }
 
             override fun onTick(millisUntilFinished: Long) {
